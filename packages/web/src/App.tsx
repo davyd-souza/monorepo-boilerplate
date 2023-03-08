edad76565bc48f907e22fe0e367a8e3d3bc7fdfc
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-08 01:19:27 -0300

    feat: implement web package

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,6 @@
+// DEPENDENCY
+import React from 'react'
+
+export function App() {
+  return <h1>Hello Monorepo</h1>
+}
---
+++ packages/web/src/index.tsx
@@ -0,0 +1,8 @@
+// DEPENDENCY
+import React from 'react'
+import { render } from 'react-dom'
+
+// COMPONENT
+import { App } from './App'
+
+render(<App />, document.getElementById('root'))
